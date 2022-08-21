0caac607b2b05d870d8307c21ddcde05fb0996b8
Eduardo Gonç <dudue0578@gmail.com>
2022-08-20 18:08:02 -1200

    Iniciando servidor e connectando MongoDB na aplicação

--- server.js
+++ server.js
@@ -3,4 +3,22 @@
    const app = express();
    const path = require('paths');
 
-   
+// Mongoose - dotenv
+   require('dotenv').config();
+   const mongoose = require('mongoose');
+   const MongoStore = require('connect-mongo');
+   // Config
+      mongoose.connect(process.env.CONNECTION_STRING, {
+         useNewUrl: true, useUnifiedTopology: true}).then(() => {
+            console.log('Flag db conected');
+            app.emit('pronto')
+         }).catch((e) => {console.log(e)});
+
+// Start server
+   const PORT = 3000;
+   app.on('pronto', () => {
+      app.listen(PORT, () => {
+         console.log(`Acessar http://localhost:${PORT}`);
+         console.log(`Servidor executando na porta ${PORT}.`)
+      });
+   });
