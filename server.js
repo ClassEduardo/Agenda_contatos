02403fb4b8f20a0faf53103bb3a48aa7af29fbbc
Eduardo Gonç <dudue0578@gmail.com>
2022-08-20 18:12:25 -1200

    Configurações básicas como urlencoded e arquivos staticos

--- server.js
+++ server.js
@@ -14,6 +14,17 @@
             app.emit('pronto')
          }).catch((e) => {console.log(e)});
 
+// Configs app
+   // Request config 
+      app.use(express.urlencoded({ extended:true }));
+
+   // File Static config
+      app.use(express.static(path.resolve(__dirname, 'public')));
+
+   // JSON
+      app.use(express.json());
+
+
 // Start server
    const PORT = 3000;
    app.on('pronto', () => {
