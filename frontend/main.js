76fc1133f8b1d3b389cf4fa3d10b37343325fe2a
Eduardo Gonç <dudue0578@gmail.com>
2022-09-09 19:46:22 -1200

    criação dos arquivos de validação de formulário via front-end

--- frontend/main.js
+++ frontend/main.js
@@ -3,4 +3,10 @@ import 'regenerator-runtime/runtime';
 
 // import './assets/css/style.css';
 
+const LoginValidationFront = require('./modules/LoginValidation');
+const ContactValidationFront = require('./modules/ContactValidation');
+
+
+
+
 console.log('Olá mundo 3');
